0f94864c0aaf3f9804f969f338343611cf52806c
SlaviXG <slavixg409@gmail.com>
2023-11-16 23:59:09 +0200

    Add basic styling

--- Project_Little_Lemon_Restaurant/little_lemon_food_menu_app/app/src/main/java/com/example/little_lemon_food_menu_app/MainActivity.kt
+++ Project_Little_Lemon_Restaurant/little_lemon_food_menu_app/app/src/main/java/com/example/little_lemon_food_menu_app/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.little_lemon_food_menu_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.little_lemon_food_menu_app.ui.theme.Little_lemon_food_menu_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainComponent()
                 }
             }
         }
@@ -30,17 +47,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Little_lemon_food_menu_appTheme {
-        Greeting("Android")
+fun MainComponent() {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF495E57))
+    ) {
+        Text(
+            text = "Little Lemon",
+            fontSize = 32.sp,
+            color = Color(0xFFF4CE14),
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
+        )
+        Text(
+            text = "Chicago",
+            fontSize = 24.sp,
+            color = Color(0xFFFFFFFF),
+            modifier = Modifier.padding(start = 20.dp)
+        )
+        Row(Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center) {
+            Button(
+                onClick = { /*TODO*/ },
+                border = BorderStroke(1.dp, Color.Red),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray,
+                    contentColor = Color.Black
+                )
+            ) {
+                Text(text = stringResource(id = R.string.Order))
+            }
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "",
+                Modifier.height(100.dp))
+        }
     }
 }
